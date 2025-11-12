a8abe51342481abfed44944f418650d9e6336fe3
hubluish <james2048@naver.com>
2025-11-12 14:37:29 +0900

    [#3]; feat: 게시글 도메인 초기 설정
    - Article 엔티티 정의
    - ArticleRepository,  ArticlerequestDto, ArticleResponseDto 정의

---
+++ src/main/java/com/example/backendmentoring/article/dto/ArticleRequestDto.java
@@ -0,0 +1,12 @@
+package com.example.backendmentoring.article.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class ArticleRequestDto {
+
+    private String title;
+    private String content;
+}
